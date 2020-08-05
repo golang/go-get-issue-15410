c4f1927620eec302faf76abc48982cf8ab689d17
Russ Cox <rsc@golang.org>
2020-08-04 23:09:50 -0400

    app: update for go114 runtime

--- app/app.go
+++ app/app.go
@@ -2,22 +2,17 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package app
+package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"strings"
 	"time"
-
-	"appengine"
-	"appengine/urlfetch"
 )
-
-func init() {
-	http.HandleFunc("/", git)
-}
 
 const out = `<!DOCTYPE html>
 <html>
@@ -27,7 +22,7 @@ const out = `<!DOCTYPE html>
 </head>
 <body>
 If you must know, this is
-<a href="https://github.com/rsc/go-get-issue-15410">https://github.com/rsc/go-get-issue-15410</a>...
+<a href="https://github.com/golang/go-get-issue-15410">https://github.com/golang/go-get-issue-15410</a>...
 </body>
 </html>
 `
@@ -50,13 +45,15 @@ func git(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "tls not allowed", 500)
 		return
 	}
+	r.URL.Scheme = "http" // since we just disallowed TLS
 	if r.URL.Path == "/" || r.URL.Path == "/pkg" || r.URL.Path == "/pkg/p" {
 		slash(w, r)
 		return
 	}
-	ctx := appengine.NewContext(r)
-	reverse := httputil.NewSingleHostReverseProxy(urlMustParse("https://github.com/rsc/go-get-issue-15410"))
-	reverse.Transport = &urlfetch.Transport{Context: ctx}
-
+	reverse := httputil.NewSingleHostReverseProxy(urlMustParse("https://github.com/golang/go-get-issue-15410"))
 	reverse.ServeHTTP(w, r)
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), http.HandlerFunc(git)))
 }
